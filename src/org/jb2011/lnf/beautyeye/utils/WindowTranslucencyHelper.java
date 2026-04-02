bcb54586d84a475a37b307e25ee216f5a7f3429f
lornwolf <lornwolf@hotmail.com>
2026-04-03 02:19:28 +0900

    tooltip显示调整。

--- src/org/jb2011/lnf/beautyeye/utils/WindowTranslucencyHelper.java
+++ src/org/jb2011/lnf/beautyeye/utils/WindowTranslucencyHelper.java
@@ -22,75 +22,75 @@ import org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper;
  * The Class WindowTranslucencyHelper.
  */
 public class WindowTranslucencyHelper {
-    private final static String UN_WINDOWS_SORRY = "I'm sorry, the Linux platform does not support transparency" +
-            ", please pay attention to the next version of BeautyEye.";
-    
-    /**
-     * Checks if is translucency supported.
-     *
-     * @return true, if is translucency supported
-     * @see <code>GraphicsDevice.isWindowTranslucencySupported(TRANSLUCENT)</code> at JDK1.7 or later
-     * @see <code>com.sun.awt.AWTUtilities.isTranslucencySupported(AWTUtilities.Translucency.TRANSLUCENT)</code> at JDK1.6_u10 or later
-     * @author lornwolf at 2013-03-20 19:00
-     * @since 3.5
-     */
-    public static boolean isTranslucencySupported() {
-        try {
-            // Java 7+ standard API (com.sun.awt.AWTUtilities removed in Java 9)
-            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-            GraphicsDevice gd = ge.getDefaultScreenDevice();
-            return gd.isWindowTranslucencySupported(GraphicsDevice.WindowTranslucency.TRANSLUCENT);
-        } catch (Exception e) {
-            if (BeautyEyeLNFHelper.debug)
-                e.printStackTrace();
-            LogHelper.debug("Exception at WindowTranslucencyHelper.isTranslucencySupported(),"+e.getMessage());
-        }
-        return false;
-    }
-    
-    /**
-     * Sets the opacity.
-     *
-     * @param w the w
-     * @param opacity the opacity
-     */
-    public static void setOpacity(Window w, float opacity) {
-        try {
-            if (!isTranslucencySupported()) {
-                LogHelper.debug("Your OS can't supported translucency.");
-                return;
-            }
-            // Java 7+ standard API: Window.setOpacity(float)
-            w.setOpacity(opacity);
-        } catch (Exception e) {
-            if (BeautyEyeLNFHelper.debug)
-                e.printStackTrace();
-            LogHelper.debug("您的JRE版本不支持每像素半透明，BeautyEye外观将不能达到最佳视觉效果哦."+e.getMessage());
-        }
-    }
-    
-    /**
-     * Sets the window opaque.
-     *
-     * @param w the w
-     * @param opaque the opaque
-     */
-    public static void setWindowOpaque(Window w, boolean opaque) {
-        try {
-            if (!isTranslucencySupported()) {
-                LogHelper.debug("Your OS can't supported translucency.");
-                return;
-            }
-            // Java 7+ standard API: set background with alpha to control opacity
-            Color bgc = w.getBackground();
-            if (bgc == null)
-                bgc = Color.black;
-            Color newBgn = new Color(bgc.getRed(), bgc.getGreen(), bgc.getBlue(), opaque ? 255 : 0);
-            w.setBackground(newBgn);
-        } catch (Exception e) {
-            if (BeautyEyeLNFHelper.debug)
-                e.printStackTrace();
-            LogHelper.debug("您的JRE版本不支持窗口透明，BeautyEye外观将不能达到最佳视觉效果哦."+e.getMessage());
-        }
-    }
+	private final static String UN_WINDOWS_SORRY = "I'm sorry, the Linux platform does not support transparency" +
+			", please pay attention to the next version of BeautyEye.";
+	
+	/**
+	 * Checks if is translucency supported.
+	 *
+	 * @return true, if is translucency supported
+	 * @see <code>GraphicsDevice.isWindowTranslucencySupported(TRANSLUCENT)</code> at JDK1.7 or later
+	 * @see <code>com.sun.awt.AWTUtilities.isTranslucencySupported(AWTUtilities.Translucency.TRANSLUCENT)</code> at JDK1.6_u10 or later
+	 * @author lornwolf at 2013-03-20 19:00
+	 * @since 3.5
+	 */
+	public static boolean isTranslucencySupported() {
+		try {
+			// Java 7+ standard API (com.sun.awt.AWTUtilities removed in Java 9)
+			GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+			GraphicsDevice gd = ge.getDefaultScreenDevice();
+			return gd.isWindowTranslucencySupported(GraphicsDevice.WindowTranslucency.TRANSLUCENT);
+		} catch (Exception e) {
+			if (BeautyEyeLNFHelper.debug)
+				e.printStackTrace();
+			LogHelper.debug("Exception at WindowTranslucencyHelper.isTranslucencySupported(),"+e.getMessage());
+		}
+		return false;
+	}
+	
+	/**
+	 * Sets the opacity.
+	 *
+	 * @param w the w
+	 * @param opacity the opacity
+	 */
+	public static void setOpacity(Window w, float opacity) {
+		try {
+			if (!isTranslucencySupported()) {
+				LogHelper.debug("Your OS can't supported translucency.");
+				return;
+			}
+			// Java 7+ standard API: Window.setOpacity(float)
+			w.setOpacity(opacity);
+		} catch (Exception e) {
+			if (BeautyEyeLNFHelper.debug)
+				e.printStackTrace();
+			LogHelper.debug("您的JRE版本不支持每像素半透明，BeautyEye外观将不能达到最佳视觉效果哦."+e.getMessage());
+		}
+	}
+	
+	/**
+	 * Sets the window opaque.
+	 *
+	 * @param w the w
+	 * @param opaque the opaque
+	 */
+	public static void setWindowOpaque(Window w, boolean opaque) {
+		try {
+			if (!isTranslucencySupported()) {
+				LogHelper.debug("Your OS can't supported translucency.");
+				return;
+			}
+			// Java 7+ standard API: set background with alpha to control opacity
+			Color bgc = w.getBackground();
+			if (bgc == null)
+				bgc = Color.black;
+			Color newBgn = new Color(bgc.getRed(), bgc.getGreen(), bgc.getBlue(), opaque ? 255 : 0);
+			w.setBackground(newBgn);
+		} catch (Exception e) {
+			if (BeautyEyeLNFHelper.debug)
+				e.printStackTrace();
+			LogHelper.debug("您的JRE版本不支持窗口透明，BeautyEye外观将不能达到最佳视觉效果哦."+e.getMessage());
+		}
+	}
 }
